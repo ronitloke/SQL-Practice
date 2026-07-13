0f7d9ec3ed754c8f68a2f57595bdc664ca9a4b56
Ronit Loke <82684588+ronitloke@users.noreply.github.com>
2026-07-13 19:18:38 +0100

    Add files via upload

---
+++ college.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE college;
+
+USE college;
+
+
+CREATE TABLE student(
+	id int PRIMARY KEY,
+    NAME varchar(50),
+    age int not null);
+	
+    
+Insert into student values(1, "Ronit", 29);
+INSERT INTO STUDENT VALUES(2, "dANNY", 28);
